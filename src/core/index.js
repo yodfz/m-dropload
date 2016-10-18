c91df4f460686e2ace6a1d0828287d0a1b8edd5e
yodfz <yodfz@qq.com>
2016-10-18 16:56:38 +0800

    go to bottom 函数防抖

--- src/core/index.js
+++ src/core/index.js
@@ -72,10 +72,14 @@ $touch = function (element, _opt) {
     function eventscroll(e) {
         // 已经在执行了，无需再次执行
         let that = this;
-        if (that.status.loading) return;
+        if (that.status.loading || that.status.bottomEvent) return;
         if (scroll.getScrollTop() + scroll.getWindowHeight() >= (scroll.getScrollHeight() - 50)) {
             // bottom event
             that.status.loading = true;
+            that.status.bottomEvent = true;
+            setTimeout(function () {
+                that.status.bottomEvent = false;
+            }, 500);
             that.downObj.css(str.o, '1', false);
             that.downObj.innerHTML = that.opt.down.template.loading;
             that.opt.down && that.opt.down.fn(callback.call(that));
