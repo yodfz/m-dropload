cbc33e7875dac60121d639e44631c8a8128bab7f
yodfz <yodfz@qq.com>
2017-01-20 11:14:19 +0800

    解决移动如果添加图片抖动问题

--- src/core/event/touchmove.js
+++ src/core/event/touchmove.js
@@ -11,7 +11,7 @@ export default function (e) {
         var mouseY = mouse.y - that.startMouse.y;
         // 解决与iScroll冲突问题
         if (scroll.getScrollTop() === 0 && mouseY > 0) {
-            e.preventDefault&&e.preventDefault();
+            e.preventDefault && e.preventDefault();
             // 判断是否固定距离,默认为一半屏幕高度
             if (mouseY > 0 && mouseY < that.opt.windowHeight) {
                 var offset = (mouseY + that.offsetY) / 2;
@@ -23,13 +23,13 @@ export default function (e) {
                 let offsetUpobjHeight = (offset - that.opt.height) / 2;
                 that.upObj.css(str.tf, str.t3d + '(0,' + (offsetUpobjHeight < 0 ? 0 : offsetUpobjHeight) + 'px,0)');
             }
-            if (mouseY > that.opt.height) {
+            if (mouseY > that.opt.height && that.upObj.innerHTML != that.opt.up.template.message) {
                 that.upObj.innerHTML = that.opt.up.template.message;
             }
         }
         // 尝试阻止移动丢失
-        if(mouse.y > (screenHeight-100)){
-            touchend.apply(that,e);
+        if (mouse.y > (screenHeight - 100)) {
+            touchend.apply(that, e);
         }
     }
 };
